3a97243f875237fd74afaf56ae7f0e530e747a59
uferesamuel <uferesamuel@gmail.com>
2020-04-14 19:53:59 +0100

    address book project

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,46 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+   MyApp()
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+             CircleAvatar(
+               radius: 50.0,
+               backgroundImage: AssetImage('images/pix.png'),
+             ),
+              Text(
+                  'Ufere Samuel',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold
+                ),
+              ),
+              Text(
+                'Mobile Developer',
+                style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold
+                ),
+              )
+
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
